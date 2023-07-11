e5739097711ccc01e23bc7b90479d52db09af0cc
Galla Syrodoeva <55047006+gallasglasses@users.noreply.github.com>
2023-07-11 12:58:42 +0200

    init commit

---
+++ TProject/Source/TProject/TProjectGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "TProjectGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TPROJECT_API ATProjectGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
